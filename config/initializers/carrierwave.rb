da719c455c8e7187e0f23529604265284b8cc85e
reebok159 <reebok159@bk.ru>
2018-10-01 12:11:18 +0300

    modify config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/google'
   config.fog_credentials = {
     provider: 'Google',
-    google_storage_access_key_id: ENV['google_storage_access_key_id'],
-    google_storage_secret_access_key: ENV['google_storage_secret_access_key']
+    google_storage_access_key_id: ENV.fetch('google_storage_access_key_id'),
+    google_storage_secret_access_key: ENV.fetch('google_storage_secret_access_key')
   }
-  config.fog_directory = ENV['google_storage_fog_directory']
+  config.fog_directory = ENV.fetch('google_storage_fog_directory')
 end
